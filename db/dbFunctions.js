8e391998b16feee44367b8f0eb53277c2fb7a84b
William Ruback <rubackw@gmail.com>
2022-02-02 16:37:17 -0500

    Added add functions and implemented them.

--- db/dbFunctions.js
+++ db/dbFunctions.js
@@ -1,5 +1,4 @@
-const connection = require("./connection.js");
-
+const inquirer = require("inquirer");
 class Database {
 
     async viewDepartments(connection) {
@@ -31,6 +30,120 @@ class Database {
         LEFT JOIN employee m ON e.manager_id  = m.id 
         ORDER BY e.id; `, (err, results) => {
                 resolve(results);
+            });
+        });
+    }
+    async addDepartment(connection) {
+        const words = await inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the new Department's name?",
+                name: "departmentName",
+            }
+        ]);
+        return await new Promise(function (resolve, Reject) {
+            connection.query(`INSERT INTO department (name)
+            VALUES (?)`, [words.departmentName], (err, results) => {
+                resolve(words.departmentName);
+            });
+        });
+    }
+    async addRole(connection) {
+        const departments = await new Promise(function (resolve, Reject) {
+            connection.query("SELECT * FROM department", (err, results) => {
+                let outputArray = [];
+                results.forEach(element => {
+                    outputArray.push({
+                        name: element.name,
+                        value: element.id
+                    });
+                });
+                resolve(outputArray);
+            });
+        }); 
+        const words = await inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the new Roles's name?",
+                name: "roleName",
+            },
+            {
+                type: "input",
+                message: "What is the new Roles's salary?",
+                name: "roleSalary",
+            },
+            {
+                type: "list",
+                message: "What is the new Roles's department?",
+                choices: departments,
+                name: "roleDepartment",
+            },
+        ]);
+        return await new Promise(function (resolve, Reject) {
+            connection.query(`INSERT INTO role (title, salary, department_id)
+            VALUES (?, ?, ?)`, [words.roleName, parseInt(words.roleSalary), words.roleDepartment], (err, results) => {
+                resolve(words.roleName);
+            });
+        });
+    }
+    async addEmployee(connection) {
+        const managers = await new Promise(function (resolve, Reject) {
+            connection.query("SELECT * FROM employee", (err, results) => {
+                let outputArray = [];
+                for (const element of results){
+                    outputArray.push({
+                        name: element.first_name + " " + element.last_name,
+                        value: element.id
+                    });
+                }
+                outputArray.unshift({
+                    name: "none",
+                    value: null
+                })
+                resolve(outputArray);
+            });
+        }); 
+        const roles = await new Promise(function (resolve, Reject) {
+            connection.query("SELECT * FROM role", (err, results) => {
+                let outputArray = [];
+                for (const element of results){
+                    outputArray.push({
+                        name: element.title,
+                        value: element.id
+                    });
+                }
+                resolve(outputArray);
+            });
+        }); 
+        const words = await inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the new Employee's first name?",
+                name: "employeeFName",
+            },
+            {
+                type: "input",
+                message: "What is the new Employee's last name?",
+                name: "employeeLName",
+            },
+            {
+                type: "list",
+                message: "What is the new Employee's Role?",
+                choices: roles,
+                name: "employeeRole",
+            },
+            {
+                type: "list",
+                message: "What is the new Employee's Manager?",
+                choices: managers,
+                name: "employeeManager",
+            },
+        ]);
+        return await new Promise(function (resolve, Reject) {
+            console.log(words);
+            connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES (?, ?, ?, ?)`, [words.employeeFName, words.employeeLName, words.employeeRole, words.employeeManager], (err, results) => {
+                resolve(words.employeeFName + " " + words.employeeLName);
             });
         });
     }
--- index.js
+++ index.js
@@ -28,6 +28,18 @@ async function questionLoop() {
                 console.log("\nemployees");
                 console.table(output);
                 break;
+            case "add a department":
+                output = await employeeDB.addDepartment(connection);
+                console.log(`Congrats! a new department named ${output} has be added!`);
+                break;
+            case "add a role":
+                output = await employeeDB.addRole(connection);
+                console.log(`Congrats! a new role named ${output} has be added!`);
+                break;
+            case "add an employee":
+                output = await employeeDB.addEmployee(connection);
+                console.log(`Congrats! a new Employee is named ${output} has be added!`);
+                break;
             case "end this program.":
                 console.log("Ending program now.");
                 process.exit();
